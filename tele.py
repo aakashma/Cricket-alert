9975de5a627f680caf74343f32dc8e6f5ef65ce8
Akash Maurya <17akashma@gmail.com>
2022-07-05 18:27:18 +0530

    Update tele.py

--- tele.py
+++ tele.py
@@ -6,7 +6,7 @@ import time
 
 from aiogram import Bot, Dispatcher, executor, types
 
-API_TOKEN = 'Your Bot Token'
+API_TOKEN = '5411289796:AAHChCpW4w-s6abo7sPYT639YSVJUkLcOrk'
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
